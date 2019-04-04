006d9a84c299c5bdf898b0fe137ab10213ab9d19
Konstantin <kostya.battlefield13@mail.ru>
2019-04-04 18:47:23 +0400

    First version

---
+++ gDatabase/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Dictionary<string, double>> data = new List<Dictionary<string, double>>();
+
+            Console.WriteLine(" How much");
+            var count = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" Name and feature");
+            for (int i = 0; i < count; i++)
+            {
+                var name = Console.ReadLine();
+                var feature = double.Parse(Console.ReadLine());
+
+                data[i].Add(name, feature);
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(data[i].Keys);
+            }
+        }
+    }
+}
